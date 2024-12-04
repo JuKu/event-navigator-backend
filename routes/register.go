42042a4c1c540f9e0ad5c81c2a73c4ceaa3ca2ad
JuKu <kuenzel.justin@t-online.de>
2024-12-05 00:52:08 +0100

    model, routes: Add cancellation for registration for events

--- routes/register.go
+++ routes/register.go
@@ -32,5 +32,28 @@ func registerForEvent(context *gin.Context) {
 }
 
 func cancelRegistrationForEvent(context *gin.Context) {
-	//
+	userId := context.GetInt64("userId")
+
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id.", "error": err.Error()})
+	}
+
+	var event model.Event
+	event.ID = eventId
+
+	/*event, err := model.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not find event by id.", "error": err.Error()})
+		return
+	}*/
+
+	err = event.CancelRegistration(userId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not cancel registration for event.", "error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "User cancelled registration for event.", "event": event})
 }
